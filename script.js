63704dde9b76699afbc8580ea9b543eb1a5a2a02
Stephen Hannon <stephenhannon4@gmail.com>
2020-05-01 10:47:19 -0500

    Spaces to tabs

--- script.js
+++ script.js
@@ -4,18 +4,18 @@
  * @param {Node} $newNode
  */
 const prependChild = ($parentNode, $newNode) => {
-    $parentNode.insertBefore($newNode, $parentNode.firstChild);
+	$parentNode.insertBefore($newNode, $parentNode.firstChild);
 }
 
 const getAndParse = (key) => {
-    const value = localStorage.getItem(key);
-    if (value === null) throw new Error(`Key ${key} not found in localStorage`);
-    try {
-        return JSON.parse(value);
-    } catch (e) {
-        console.error(`Key ${key} has invalid JSON ${value}`);
-        throw e;
-    }
+	const value = localStorage.getItem(key);
+	if (value === null) throw new Error(`Key ${key} not found in localStorage`);
+	try {
+		return JSON.parse(value);
+	} catch (e) {
+		console.error(`Key ${key} has invalid JSON ${value}`);
+		throw e;
+	}
 }
 
 /**
@@ -23,31 +23,31 @@ const getAndParse = (key) => {
  * @param {HTMLElement} $el
  */
 const setHeight = ($el) => {
-    $el.style.height = Math.max($el.scrollHeight, 200) + 'px';
+	$el.style.height = Math.max($el.scrollHeight, 200) + 'px';
 }
 
 const updateData = (index, property, value) => {
 	if (index === Number(localStorage.getItem('nextId'))) {
-        const data = {
+		const data = {
 			title: '',
 			body: '',
 			[property]: value,
-        };
+		};
 		localStorage.setItem(index, JSON.stringify(data));
-        const ids = getAndParse('ids');
-        ids.push(index);
-        localStorage.setItem('ids', JSON.stringify(ids));
+		const ids = getAndParse('ids');
+		ids.push(index);
+		localStorage.setItem('ids', JSON.stringify(ids));
 		localStorage.setItem('nextId', index + 1);
 		createNote(index + 1);
 	} else {
-        const data = getAndParse(index);
-        data[property] = value;
-        localStorage.setItem(index, JSON.stringify(data));
+		const data = getAndParse(index);
+		data[property] = value;
+		localStorage.setItem(index, JSON.stringify(data));
 	}
 }
 
 const createNote = (index, { title, body } = { title: '', body: '' }) => {
-    const isBlank = (index === Number(localStorage.getItem('nextId')));
+	const isBlank = (index === Number(localStorage.getItem('nextId')));
 
 	const $note = document.createElement('div');
 	$note.className = 'note';
@@ -57,28 +57,28 @@ const createNote = (index, { title, body } = { title: '', body: '' }) => {
 	$title.placeholder = 'Title'
 	$title.addEventListener('input', (event) => {
 		updateData(index, 'title', event.target.value);
-    });
+	});
 
-    const $delete = document.createElement('button');
-    $delete.textContent = 'delete';
-    $delete.disabled = isBlank; // TODO: enable this after saving the note
-    $delete.addEventListener('click', (event) => {
-        const ids = getAndParse('ids');
-        const idsIndex = ids.indexOf(index);
-        ids.splice(idsIndex, 1);
-        localStorage.setItem('ids', JSON.stringify(ids));
+	const $delete = document.createElement('button');
+	$delete.textContent = 'delete';
+	$delete.disabled = isBlank; // TODO: enable this after saving the note
+	$delete.addEventListener('click', (event) => {
+		const ids = getAndParse('ids');
+		const idsIndex = ids.indexOf(index);
+		ids.splice(idsIndex, 1);
+		localStorage.setItem('ids', JSON.stringify(ids));
 
-        localStorage.removeItem(index);
-        event.target.parentNode.remove();
-    });
+		localStorage.removeItem(index);
+		event.target.parentNode.remove();
+	});
 
 	const $body = document.createElement('textarea');
 	$body.value = body;
-    $body.placeholder = 'Start typing...';
-    setTimeout(setHeight, 0, $body);
+	$body.placeholder = 'Start typing...';
+	setTimeout(setHeight, 0, $body);
 	$body.addEventListener('input', (event) => {
-        $body.style.height = 'auto';
-        setHeight($body);
+		$body.style.height = 'auto';
+		setHeight($body);
 		updateData(index, 'body', event.target.value);
 	});
 
@@ -86,16 +86,16 @@ const createNote = (index, { title, body } = { title: '', body: '' }) => {
 	$note.appendChild($delete);
 	$note.appendChild($body);
 	prependChild(document.getElementById('notes'), $note);
-    if (isBlank) {
-        $body.focus();
-    }
+	if (isBlank) {
+		$body.focus();
+	}
 }
 
 const initStorage = () => {
-    if (localStorage.getItem('ids') === null) {
-        localStorage.setItem('ids', '[]');
-        localStorage.setItem('nextId', '0');
-    }
+	if (localStorage.getItem('ids') === null) {
+		localStorage.setItem('ids', '[]');
+		localStorage.setItem('nextId', '0');
+	}
 }
 
 const initNotes = () => {
